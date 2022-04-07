82f1b6d3c4403a729a7f46a6bd28bca12b2fd15f
Neeiser <stortini.study@gmail.com>
2022-04-07 15:34:38 +0200

    inistial commit test structuring

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quam ipsa aliquam non enim doloribus perspiciatis expedita minima deserunt nam, quisquam, aperiam nihil eveniet modi officiis atque ipsum nostrum commodi?</p>
+    <?php
+        
+        echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quam ipsa aliquam non enim doloribus perspiciatis expedita minima deserunt nam, quisquam, aperiam nihil eveniet modi officiis atque ipsum nostrum commodi?'
+    ?>
+</body>
+</html>
+
+<style>
+
+</style>
